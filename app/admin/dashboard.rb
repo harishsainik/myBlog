fccab0dcf36b2b8a753b491f8f627186c9157801
Anshu Kumar <anshu.kumar@ebizontek.com>
2018-10-20 19:45:53 +0530

    Active admin

---
+++ app/admin/dashboard.rb
@@ -0,0 +1,38 @@
+ActiveAdmin.register_page "Dashboard" do
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Recent Posts" do
+          table_for Post.order("id desc").limit(15) do
+            column :id
+            column :title do |post|
+              link_to post.title, [:admin, post]
+            end
+            column :category,:sortable => :category
+            column :created_at
+          end
+          strong (link_to "Show All Posts" , :posts )
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "Categories" do
+          table_for Category.order("id desc").limit(15) do
+      			column :id
+      			column "Category Title",:title do |category|
+      				link_to category.title,[:admin,category]
+      			end
+      			column :created_at
+      		end
+      		strong {link_to "Show All Categories",:admin_categories}
+        end
+      end
+    end
+
+  end
+
+
+
+end

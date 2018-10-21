ef11d94d4280238ae439da10b411d9dd3051065e
Anshu Kumar <anshu.kumar@ebizontek.com>
2018-10-21 13:27:03 +0530

    dnfkd

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Category do
   menu :label => "Blog Categories",:priority => 3
+  permit_params :title 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
